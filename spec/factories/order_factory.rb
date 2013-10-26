8dbeb277e6f6adca912f6244d8c53a0d6175a38a
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-10-25 22:03:27 -0400

    Should be an h3 to match other styles.

--- spec/factories/order_factory.rb
+++ spec/factories/order_factory.rb
@@ -1,33 +1,22 @@
 FactoryGirl.define do
 
   factory :order_in_address_state, :parent => :order do
-    after_build { |order|
-      li = FactoryGirl.create(:line_item, :order => order)
-      order.line_items << li
+    after(:build) { |order|
+      order.line_items << FactoryGirl.create(:line_item)
     }
 
-    after_create { |order|
+    after(:create) { |order|
       order.next!
     }
   end
 
   factory :order_in_delivery_state, :parent => :order_in_address_state do
-    after_create { |order| order.next! }
-  end
-
-  factory :order_in_payment_state, :parent => :order_in_delivery_state do
-    after_create { |order| 
-      order.next! 
+    after(:create) { |order|
+      create(:state, :country => create(:country))
+      create(:shipping_method)
+      create(:stock_location)
+      order.next!
     }
   end
 
-  factory :order_in_confirm_state, :parent => :order_in_payment_state do
-    after_create do |order|
-      order.next!
-    end
-  end
-
-  factory :order_in_complete_state, :parent => :order_in_confirm_state do
-    after_create { |order| order.next! && order.finalize! }
-  end
 end
--- spec/features/pay_with_credit_cards_spec.rb
+++ spec/features/pay_with_credit_cards_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
-describe "PayWithCreditCards" do
+describe "PayWithCreditCards", :js => true do
+
   describe "GET /checkout/payment" do
 
+    let!(:country) { create(:country) }
+    let!(:state) { create(:state, :country => country) }
+    let!(:shipping_method) { create(:shipping_method) }
+    let!(:stock_location) { create(:stock_location) }
+    let!(:mug) { create(:product, :name => "RoR Mug") }
     let (:user) { create(:user) }
-    
+
     before(:each) do
       @bogus_payment_method = create(:bogus_payment_method, :display_on => :front_end)
       create(:payment_method, :display_on => :front_end)
-
-      shipping_method = create(:shipping_method)
-      Spree::ShippingMethod.stub(:all_available) { [shipping_method] }
 
       sign_in_as!(user)
     end
@@ -19,10 +22,6 @@ describe "PayWithCreditCards" do
       subject { page }
 
       before do
-        create(:product)
-        create(:country)
-        create(:state)
-
         Spree::CreditCard.all.map(&:destroy)
 
         visit spree.products_path
@@ -79,31 +78,57 @@ describe "PayWithCreditCards" do
     context "existing cards" do
       before(:each) do
 
-        # set up existing payments with this credit card
-        @credit_card = create(:credit_card, :gateway_payment_profile_id => 'FAKE_GATEWAY_ID')
+        # # set up existing payments with this credit card
+        # @credit_card = create(:credit_card, :gateway_payment_profile_id => 'FAKE_GATEWAY_ID')
+        # 
+        # order = create(:order_in_delivery_state, :user => user)
+        # order.update!  # set order.total
+        # 
+        # # go to payment state
+        # order.next
+        # order.state.should eq('payment')
+        # 
+        # # add a payment 
+        # payment = create(:payment, :order => order, :source =>  @credit_card, :amount => order.total, :payment_method => @bogus_payment_method)
+        # 
+        # # go to confirm
+        # order.next
+        # order.state.should eq('confirm')
+        # 
+        # # go to complete
+        # order.next
+        # order.state.should eq('complete')
+        # 
+        # # capture payment
+        # order.payment.capture!
+        # order.update!
+        # order.should_not be_outstanding_balance
 
-        order = create(:order_in_delivery_state, :user => user)
-        order.update!  # set order.total
+        visit spree.products_path
 
-        # go to payment state
-        order.next
-        order.state.should eq('payment')
+        find(:xpath, "//div[@class='product-image']/a").click
+        click_button 'Add To Cart'
+        click_button 'Checkout'
+        fill_in 'order_bill_address_attributes_firstname', :with => 'Jeff'
+        fill_in 'order_bill_address_attributes_lastname', :with => 'Squires'
+        fill_in 'order_bill_address_attributes_address1', :with => '123 Foo St'
+        fill_in 'order_bill_address_attributes_city', :with => 'Fooville'
+        select 'Alabama', :from => 'order_bill_address_attributes_state_id'
+        fill_in 'order_bill_address_attributes_zipcode', :with => '12345'
+        fill_in 'order_bill_address_attributes_phone', :with => '123-123-1234'
+        check "Use Billing Address"
 
-        # add a payment 
-        payment = create(:payment, :order => order, :source =>  @credit_card, :amount => order.total, :payment_method => @bogus_payment_method)
+        click_button 'Save and Continue'
+        click_button "Save and Continue"
 
-        # go to confirm
-        order.next
-        order.state.should eq('confirm')
+        fill_in "Card Number", :with => '4111111111111111'
+        fill_in "Card Code", :with => '111'
 
-        # go to complete
-        order.next
-        order.state.should eq('complete')
+        click_button "Save and Continue"
+        click_button "Place Order"
+        page.should have_content('Your order has been processed successfully')
 
-        # capture payment
-        order.payment.capture!
-        order.update!
-        order.should_not be_outstanding_balance
+        @credit_card = Spree::Payment.last.source
       end
 
       it "allows an existing credit card to be chosen from list and used for a purchase" do
@@ -116,8 +141,7 @@ describe "PayWithCreditCards" do
         fill_in 'order_bill_address_attributes_lastname', :with => 'Squires'
         fill_in 'order_bill_address_attributes_address1', :with => '123 Foo St'
         fill_in 'order_bill_address_attributes_city', :with => 'Fooville'
-        fill_in 'order_bill_address_attributes_state_name',:with => 'Alabama'
-                                    
+        select 'Alabama', :from => 'order_bill_address_attributes_state_id'
         fill_in 'order_bill_address_attributes_zipcode', :with => '12345'
         fill_in 'order_bill_address_attributes_phone', :with => '123-123-1234'
         check "Use Billing Address"
@@ -126,7 +150,8 @@ describe "PayWithCreditCards" do
 
         click_button 'Save and Continue'
 
-        page.should have_xpath("//table[@class='existing-credit-card-list']/tbody/tr", :text => @credit_card.last_digits) #, :count => x) 
+        sleep 10
+        page.should have_xpath("//table[@class='existing-credit-card-list']/tbody/tr", :text => @credit_card.last_digits)
         choose 'existing_card'
 
         click_button 'Save and Continue'
@@ -134,7 +159,7 @@ describe "PayWithCreditCards" do
         page.should have_content "Ending in #{@credit_card.last_digits}"
       end
 
-      it 'allows selecting a different payment method', :js => true do
+      it 'allows selecting a different payment method' do
         visit spree.products_path
 
         find(:xpath, "//div[@class='product-image']/a").click
